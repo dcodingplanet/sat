d309c0142642af011159b8b0496f403e630cf2a1
Daniel Schwarz <daniel8282@gmx.net>
2015-03-16 16:47:15 +0100

    adding a new folder named js and a file
    - Globals.js is a file describing the DCP namespace

---
+++ js/Globals.js
@@ -0,0 +1,45 @@
+/** 
+ * Dies ist eine Datei mit globalen Variablen im DCP Namensraum
+ */
+var DCP = DCP || {};
+
+/**
+ * Die Farben
+ * @type Object
+ */
+DCP.Colors = {};
+/**
+ * @type String die Standardhintergrundfarbe fuer den Canvas
+ */
+DCP.Colors.BG_COLOR = '#FFFFFF';
+/**
+ * @type String die Standardhintergrundfarbe fuer den Canvas
+ */
+DCP.Colors.FG_COLOR = '#000000';
+
+DCP.Colors.BLUE = '#0000FF';
+DCP.Colors.GREEN = '#00FF00';
+DCP.Colors.YELLOW = '#FFFF00';
+DCP.Colors.RED = '#FF0000';
+
+
+/**
+ * Die MessInstrumente
+ * @type Object
+ */
+DCP.Measures = {};
+/**
+ * @type Number die Standardbreite des Canvas
+ */
+DCP.Measures.CAN_WIDTH = 800;
+/**
+ * @type Number die Standardhoehe des Canvas
+ */
+DCP.Measures.CAN_HEIGHT = 600;
+
+DCP.Measures.PI_2 = 2 * Math.PI;
+
+DCP.Measures.MASS_MAX = 1000;
+
+
+
